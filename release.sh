d41b8b48fb12484d506a95c5f9195742e001a714
Paul Thrasher <thrashr888@gmail.com>
2018-11-30 10:30:19 -0800

    Add a release script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  echo "Releases a new version of the Sentinel Docker image."
+  echo "Example:"
+  echo "    ./release.sh v0.6.0"
+  exit 1
+fi
+
+SENTINEL_VERSION=$1
+
+git add .
+git commit -m"upgrade to $SENTINEL_VERSION"
+git tag ${SENTINEL_VERSION}
+git push
+git push origin ${SENTINEL_VERSION}
