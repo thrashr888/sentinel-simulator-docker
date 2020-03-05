e350fe1dec8d755c7fce1cb885d328ae31a42bdd
Paul Thrasher <pthrasher@hashicorp.com>
2020-03-05 14:23:32 -0800

    upgrade to v0.14.4

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]
 then
   echo "Releases a new version of the Sentinel Docker image."
   echo "Example:"
-  echo "    ./release.sh v0.14.3"
+  echo "    ./release.sh v0.14.4"
   exit 1
 fi
 
